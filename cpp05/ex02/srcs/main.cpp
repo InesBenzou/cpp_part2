d336e8568b0443bdce508bf73d7972e22fac9b8b
ibenzian <benziane.ines@gmail.com>
2024-11-28 19:20:03 +0100

    28-11-19-19

--- cpp05/ex02/srcs/main.cpp
+++ cpp05/ex02/srcs/main.cpp
@@ -15,50 +15,60 @@ int main (void)
 
 
 	// //SHRUBBERY CREATION FORM 
+	std::cout << "\033[33mSHRUBBERRY FORM\033[0m" << std::endl;
 	ShrubberyCreationForm shrubbery("home");
 	std::cout << shrubbery << std::endl;
 	jean.signForm(shrubbery);
-	// std::cout << shrubbery << std::endl;
-	// shrubbery.execute(jean);
+	std::cout << shrubbery << std::endl;
+	shrubbery.execute(jean);
+	std::cout << std::endl;
+	
+	// ROBTOMY REQUEST FORM 
+	std::cout << "\033[33mROBTOMY REQUEST FORM\033[0m" << std::endl;
+	RobotomyRequestForm request("Robot");
+	std::cout << request << std::endl;
+	try
+	{
+		jean.signForm(request);
+		std::cout << request << std::endl;
+		request.execute(jean);
+	}
 
-	//ROBTOMY REQUEST FORM 
-	// RobotomyRequestForm request("Robot");
-	// std::cout << request << std::endl;
-	// try
-	// {
-	// 	jean.signForm(request);
-	// 	std::cout << request << std::endl;
-	// 	request.execute(jean);
-	// }
+	catch (const FormNotSignedException& e)
+	{
+        std::cerr << e.what() << std::endl;
+    }
+	catch (const std::exception& e)
+	{
+        std::cerr << "Autre exception attrapée : " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
 
-	// catch (const FormNotSignedException& e)
-	// {
-    //     std::cerr << e.what() << std::endl;
-    // }
-	// catch (const std::exception& e)
-	// {
-    //     std::cerr << "Autre exception attrapée : " << e.what() << std::endl;
-	// }
+	//PRESIDENTIAL PARDON FORM
+	std::cout << "\033[33mPRESIDENTIAL PARDON FORM\033[0m" << std::endl;
+	PresidentialPardonForm pardon("Mr Smith ");
+	std::cout << pardon << std::endl;
+	try
+	{
+		jean.signForm(pardon);
+		std::cout << pardon << std::endl;
+		pardon.execute(jean);
+	}
+	catch (const FormNotSignedException& e)
+	{
+        std::cerr << e.what() << std::endl;
+    }
+	catch (const std::exception& e)
+	{
+        std::cerr << "Autre exception attrapée : " << e.what() << std::endl;
+	}
+	std::cout << std::endl;
 
-	// //PRESIDENTIAL PARDON FORM
-	// PresidentialPardonForm pardon("Mr Smith ");
-	// std::cout << pardon << std::endl;
-	// try
-	// {
-	// 	jean.signForm(pardon);
-	// 	std::cout << pardon << std::endl;
-	// 	pardon.execute(jean);
-	// }
-	// catch (const FormNotSignedException& e)
-	// {
-    //     std::cerr << e.what() << std::endl;
-    // }
-	// catch (const std::exception& e)
-	// {
-    //     std::cerr << "Autre exception attrapée : " << e.what() << std::endl;
-	// }
+	//TESTER EXECUTE FORM
+	std::cout << "\033[33mEXECUTE FORM\033[0m" << std::endl;
+	jean_bis.executeForm(shrubbery);
+	jean_bis.executeForm(pardon);
+	jean_bis.executeForm(request);
 
-	// //TESTER EXECUTE FORM
-	// jean_bis.executeForm(shrubbery);
 	return 0;
 }
